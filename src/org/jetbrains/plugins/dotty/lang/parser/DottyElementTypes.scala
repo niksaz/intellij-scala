fb2d5297c65499d3038bdbd01eb63e869992a5b7
niksaz <nikitasazanovich@gmail.com>
2017-03-27 01:31:51 +0300

    dotty parser fixed

--- src/org/jetbrains/plugins/dotty/lang/parser/DottyElementTypes.scala
+++ src/org/jetbrains/plugins/dotty/lang/parser/DottyElementTypes.scala
@@ -1,10 +1,14 @@
 package org.jetbrains.plugins.dotty.lang.parser
 
 import com.intellij.lang.ASTNode
-import com.intellij.psi.PsiElement
+import com.intellij.psi.stubs.PsiFileStub
+import com.intellij.psi.tree.IStubFileElementType
+import com.intellij.psi.{PsiElement, PsiFile}
+import org.jetbrains.plugins.dotty.DottyLanguage
 import org.jetbrains.plugins.dotty.lang.psi.impl.base.types.{DottyAndTypeElementImpl, DottyRefinedTypeElementImpl, DottyTypeArgumentNameElementImpl}
 import org.jetbrains.plugins.scala.lang.lexer.ScalaElementType
 import org.jetbrains.plugins.scala.lang.parser.ScalaPsiCreator.SelfPsiCreator
+import org.jetbrains.plugins.scala.lang.psi.stubs.elements.ScStubFileElementType
 
 /**
   * @author adkozlov
@@ -20,4 +24,7 @@ object DottyElementTypes {
   val TYPE_ARGUMENT_NAME = new ScalaElementType("Dotty type argument name") with SelfPsiCreator {
     override def createElement(node: ASTNode): PsiElement = new DottyTypeArgumentNameElementImpl(node)
   }
+
+  val FILE: IStubFileElementType[_ <: PsiFileStub[_ <: PsiFile]] =
+    new ScStubFileElementType(language = DottyLanguage.INSTANCE)
 }
--- src/org/jetbrains/plugins/scala/lang/parser/ScalaParserDefinition.scala
+++ src/org/jetbrains/plugins/scala/lang/parser/ScalaParserDefinition.scala
@@ -8,9 +8,11 @@ import com.intellij.psi.stubs.PsiFileStub
 import com.intellij.psi.tree.{IStubFileElementType, TokenSet}
 import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.psi.{FileViewProvider, PsiElement, PsiFile}
+import org.jetbrains.plugins.dotty.lang.parser.{DottyParser, DottyPsiCreator}
 import org.jetbrains.plugins.scala.lang.lexer.{ScalaLexer, ScalaTokenTypes}
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.imports.ScImportStmt
 import org.jetbrains.plugins.scala.lang.psi.impl.ScalaFileImpl
+import org.jetbrains.plugins.scala.project.ProjectExt
 import org.jetbrains.plugins.scala.settings._
 
 /**
@@ -18,15 +20,17 @@ import org.jetbrains.plugins.scala.settings._
   */
 class ScalaParserDefinition extends ScalaParserDefinitionWrapper {
 
-  protected val psiCreator: PsiCreator = ScalaPsiCreator
+  var hasDotty = false
 
   def createLexer(project: Project): ScalaLexer = {
     val settings = ScalaProjectSettings.getInstance(project)
     new ScalaLexer(settings.isTreatDocCommentAsBlockComment)
   }
 
-  def createParser(project: Project): ScalaParser =
-    new ScalaParser
+  def createParser(project: Project): ScalaParser = {
+    hasDotty = project.hasDotty
+    if (hasDotty) new DottyParser else new ScalaParser
+  }
 
   def getFileNodeType: IStubFileElementType[_ <: PsiFileStub[_ <: PsiFile]] =
     ScalaElementTypes.FILE
@@ -38,7 +42,7 @@ class ScalaParserDefinition extends ScalaParserDefinitionWrapper {
   def getWhitespaceTokens: TokenSet = ScalaTokenTypes.WHITES_SPACES_TOKEN_SET
 
   def createElement(astNode: ASTNode): PsiElement =
-    psiCreator.createElement(astNode)
+    (if (hasDotty) DottyPsiCreator else ScalaPsiCreator).createElement(astNode)
 
   def createFile(fileViewProvider: FileViewProvider): PsiFile = {
     ScalaFileFactory.EP_NAME.getExtensions
