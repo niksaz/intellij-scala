3931fa31d92099f3616c03f02ec9010e4747613c
niksaz <nikitasazanovich@gmail.com>
2017-04-04 08:53:00 +0300

    XmlPatterns transformed into interpolated strings

--- src/org/jetbrains/plugins/dotty/codeInspection/xml/XmlPatternsNotSupportedInspection.scala
+++ src/org/jetbrains/plugins/dotty/codeInspection/xml/XmlPatternsNotSupportedInspection.scala
@@ -4,34 +4,34 @@ import com.intellij.codeInspection.ProblemHighlightType.ERROR
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
-import org.jetbrains.plugins.dotty.codeInspection.xml.XmlExprIsNotSupportedInspection._
+import org.jetbrains.plugins.dotty.codeInspection.xml.XmlPatternsNotSupportedInspection._
 import org.jetbrains.plugins.scala.codeInspection.{AbstractFixOnPsiElement, AbstractInspection, InspectionBundle}
 import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypesEx
 import org.jetbrains.plugins.scala.lang.psi.ScalaPsiElement
 import org.jetbrains.plugins.scala.lang.psi.impl.ScalaPsiElementFactory
-import org.jetbrains.plugins.scala.lang.psi.impl.expr.xml._
+import org.jetbrains.plugins.scala.lang.psi.impl.expr.xml.ScXmlPatternImpl
 
 /**
   * @author niksaz
   */
-class XmlExprIsNotSupportedInspection extends AbstractInspection(id, name) {
+class XmlPatternsNotSupportedInspection extends AbstractInspection(id, name) {
   override def actionFor(holder: ProblemsHolder): PartialFunction[PsiElement, Any] = {
-    case expr: ScXmlExprImpl =>
-      holder.registerProblem(expr, message, ERROR, new ReplaceXmlExprQuickFix(expr))
+    case pattern: ScXmlPatternImpl =>
+      holder.registerProblem(pattern, message, ERROR, new ReplaceXmlPatternQuickFix(pattern))
   }
 }
 
-class ReplaceXmlExprQuickFix(token: PsiElement) extends AbstractFixOnPsiElement(name, token) {
-  def replaceXmlExpr(psiElem: ScalaPsiElement): Unit = {
+class ReplaceXmlPatternQuickFix(token: PsiElement) extends AbstractFixOnPsiElement(name, token) {
+  def replaceXmlPattern(psiElem: ScalaPsiElement): Unit = {
     for (e <- psiElem.getChildren) {
       e match {
         case sc: ScalaPsiElement =>
-          replaceXmlExpr(sc)
+          replaceXmlPattern(sc)
         case _ =>
       }
     }
     psiElem match {
-      case _: ScXmlExprImpl =>
+      case _: ScXmlPatternImpl =>
         val anchorNode = psiElem.getNode.getFirstChildNode
         val xmlPrefix = ScalaPsiElementFactory.createInterpolatedStringPrefix("xml")(psiElem.getManager)
         val startQuotes = ScalaPsiElementFactory.createMultilineInterpolatedStringEnd(psiElem.getManager)
@@ -40,23 +40,23 @@ class ReplaceXmlExprQuickFix(token: PsiElement) extends AbstractFixOnPsiElement(
         val endQuotes = ScalaPsiElementFactory.createMultilineInterpolatedStringEnd(psiElem.getManager)
         psiElem.getNode.addChild(endQuotes)
       case _ =>
-        val injectoionNode = psiElem.getNode.findChildByType(ScalaTokenTypesEx.SCALA_IN_XML_INJECTION_START)
-        if (injectoionNode != null) {
-          psiElem.getNode.addChild(
-            ScalaPsiElementFactory.createInterpolatedStringInjection(psiElem.getManager), injectoionNode)
-        }
+    }
+    val injectoionNode = psiElem.getNode.findChildByType(ScalaTokenTypesEx.SCALA_IN_XML_INJECTION_START)
+    if (injectoionNode != null) {
+      psiElem.getNode.addChild(
+        ScalaPsiElementFactory.createInterpolatedStringInjection(psiElem.getManager), injectoionNode)
     }
   }
 
   override def doApplyFix(project: Project): Unit = getElement match {
-    case expr: ScXmlExprImpl =>
-      replaceXmlExpr(expr)
+    case pattern: ScXmlPatternImpl =>
+      replaceXmlPattern(pattern)
     case _ =>
   }
 }
 
-object XmlExprIsNotSupportedInspection {
-  private[xml] val id = "XmpExprIsNotSupported"
+object XmlPatternsNotSupportedInspection {
+  private[xml] val id = "XmlPatternsAreNotSupported"
   private[xml] val name = InspectionBundle.message("replace.with.interpolated.string")
-  private[xml] val message = "Xml expressions are not supported in Dotty"
+    private[xml] val message = "Xml patterns are not supported in Dotty"
 }
